1b24510de5d31977449a9cae75c15d3cbeab5116
Tim <tim.fromentin.dev.pro@gmail.com>
2025-02-04 19:32:46 +0100

    l

---
+++ src/MainNav.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ('./styles/global.css')
+import ('./styles/MainNav.css')
+
+const MainNav = () => {
+    return (
+        <nav id={'mainNav'}>
+            <ul className={'mn_links'}>
+                <li className={'mn_link'}>
+                    <a href={'#'}>Acceuil</a>
+                </li>
+
+                <li className={'mn_link'}>
+                    <a href={'#'}>Produits</a>
+                </li>
+
+                <li className={'mn_link'}>
+                    <a href={'#'}>OTHER</a>
+                </li>
+                <li className={'mn_link'}>
+                    <a href={'#'}>OTHER</a>
+                </li>
+                <li className={'mn_link'}>
+                    <a href={'#'}>OTHER</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default MainNav;
